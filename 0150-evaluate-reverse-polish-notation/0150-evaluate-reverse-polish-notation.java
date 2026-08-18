60f7db6e71bd032b7ca4bd00a2617a912edb7caa
SRIGANTH-K <sriganthkofficial@gmail.com>
2026-08-18 12:00:47 +0530

    Time: 6 ms (87.13%), Space: 44.9 MB (97.59%) - LeetHub

--- 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.java
+++ 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.java
@@ -1,22 +1,30 @@
 class Solution {
     public int evalRPN(String[] tokens) {
-        int[] stack = new int[tokens.length];
-        int top = -1; // Pointer to the top of our simulated stack
-
-        for (String s : tokens) {
-            switch (s) {
-                // For operators, we combine the top two elements and shrink the stack pointer by 1
-                case "+" -> { stack[top - 1] += stack[top]; top--; }
-                case "-" -> { stack[top - 1] -= stack[top]; top--; }
-                case "*" -> { stack[top - 1] *= stack[top]; top--; }
-                case "/" -> { stack[top - 1] /= stack[top]; top--; }
-                
-                // For numbers, we expand the stack pointer and insert the number
-                default  -> stack[++top] = Integer.parseInt(s);
+        Stack <Integer> st = new Stack<>();
+        for (int i=0;i<tokens.length;i++){
+            if (tokens[i].equals("+")){
+                int b = st.pop();
+                int a = st.pop();
+                st.push(a+b);
+            }
+            else if (tokens[i].equals("-")){
+                int b = st.pop();
+                int a = st.pop();
+                st.push(a-b);
+            }
+            else if (tokens[i].equals("*")){
+                int b = st.pop();
+                int a = st.pop();
+                st.push(a*b);
+            }
+            else if (tokens[i].equals("/")){
+                int b = st.pop();
+                int a = st.pop();
+                st.push(a/b);
+            }else{
+                st.push(Integer.parseInt(tokens[i]));
             }
         }
-
-        // The final result will be the only item left, sitting at index 0
-        return stack[0];
+        return st.pop();
     }
 }
