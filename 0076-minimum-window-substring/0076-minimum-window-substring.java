616e3b77e23f7c6a11745f582700256d8d0184d9
SRI GANTH K <sriganthkofficial@gmail.com>
2026-08-31 09:37:36 +0530

    Time: 24 ms (22.67%), Space: 47.8 MB (23.5%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.java
+++ 0076-minimum-window-substring/0076-minimum-window-substring.java
@@ -1,41 +1,54 @@
 class Solution {
     public String minWindow(String s, String t) {
-        if (s == null || t == null || s.length() < t.length()) return "";
-
-        int[] targetCounts = new int[128];
-        for (char c : t.toCharArray()) {
-            targetCounts[c]++;
+        if (s == null || t == null || s.length() < t.length()) {
+            return "";
         }
 
-        int left = 0, right = 0;
+        // Frequency map to store the required counts of characters in t
+        Map<Character, Integer> targetMap = new HashMap<>();
+        for (char c : t.toCharArray()) {
+            targetMap.put(c, targetMap.getOrDefault(c, 0) + 1);
+        }
+
+        // Number of unique characters in t that need to match in the current window
+        int requiredMatches = targetMap.size();
+        int formedMatches = 0;
+
+        // Map to keep track of characters in the current window of s
+        Map<Character, Integer> windowMap = new HashMap<>();
+
+        // Pointers and result trackers
+        int left = 0;
         int minLength = Integer.MAX_VALUE;
-        int minLeft = 0;
-        int requiredCount = t.length();
+        int minLeft = 0; // Tracks the starting index of the best window
 
-        while (right < s.length()) {
+        for (int right = 0; right < s.length(); right++) {
             char rightChar = s.charAt(right);
-            
-            // If the character is part of 't', decrease our target requirement
-            if (targetCounts[rightChar] > 0) {
-                requiredCount--;
-            }
-            targetCounts[rightChar]--; // Becomes negative for non-t characters or excess
-            right++;
+            windowMap.put(rightChar, windowMap.getOrDefault(rightChar, 0) + 1);
 
-            // When a valid window is found, shrink from the left
-            while (requiredCount == 0) {
-                if (right - left < minLength) {
-                    minLength = right - left;
+            // If the current character match criteria for 't', increment matches
+            if (targetMap.containsKey(rightChar) && 
+                windowMap.get(rightChar).equals(targetMap.get(rightChar))) {
+                formedMatches++;
+            }
+
+            // Try to contract the window from the left when a valid window is found
+            while (formedMatches == requiredMatches) {
+                // Update the minimum window coordinates
+                if (right - left + 1 < minLength) {
+                    minLength = right - left + 1;
                     minLeft = left;
                 }
 
                 char leftChar = s.charAt(left);
-                targetCounts[leftChar]++;
-                
-                // If it becomes positive, it means we lack a necessary character for 't'
-                if (targetCounts[leftChar] > 0) {
-                    requiredCount++;
+                windowMap.put(leftChar, windowMap.get(leftChar) - 1);
+
+                // If removing leftChar breaks the match criteria, decrement matches
+                if (targetMap.containsKey(leftChar) && 
+                    windowMap.get(leftChar) < targetMap.get(leftChar)) {
+                    formedMatches--;
                 }
+
                 left++;
             }
         }
